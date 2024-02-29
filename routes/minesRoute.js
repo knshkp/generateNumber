81cf7fe3743116303d1d42ecede8490e9ba423d3
Kanishk Pareek <knshkpareek@gmail.com>
2024-03-01 00:24:32 +0530

    added rewards in mines

--- routes/minesRoute.js
+++ routes/minesRoute.js
@@ -7,4 +7,5 @@ const minesController=require('../controllers/minesController.js');
 mines_route.get('/getMines', minesController.getMatrix);
 mines_route.post('/sendMinesMoney', minesController.sendMinesMoney);
 mines_route.post('/receiveMinesMoney', minesController.receiveMinesMoney);
+mines_route.get(`/getReward`,minesController.getReward)
 module.exports = mines_route;
